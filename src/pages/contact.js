30de1458d4de2f5af5e3e28352274a0f6a3a8b52
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-10-07 00:05:43 +0530

    tt

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,8 +12,11 @@ export default function Contact() {
   }
 
   const sendToSanity = () => {
-    fetch('https://determined-elion-16345f.netlify.app/.netlify/functions/createlead', {
+    fetch('https://determined-elion-16345f.netlify.app/functions/createlead', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         _type: 'form',
         name: form.name,
