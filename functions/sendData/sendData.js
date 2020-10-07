da84c0aa102d034c21aefa68383296706e1de031
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-10-07 22:54:19 +0530

    test Sanity

--- functions/sendData/sendData.js
+++ functions/sendData/sendData.js
@@ -1,16 +1,15 @@
 const sanityClient = require('@sanity/client');
 
-const client = sanityClient({
-  projectId: process.env.SANITY_PROJ,
-  dataset: process.env.SANITY_DS,
-  token: process.env.SANITY_TOKEN,
-  useCdn: false
-});
-
-
 exports.handler = async(event, context) => {
   const {_type, name, email, message} = JSON.parse(event.body);
   console.log(JSON.parse(event.body));
+  const client = sanityClient({
+    projectId: process.env.SANITY_PROJ,
+    dataset: process.env.SANITY_DS,
+    token: process.env.SANITY_TOKEN,
+    useCdn: false
+  });
+  
   try {
     client.create({
       _type: _type,
@@ -18,7 +17,7 @@ exports.handler = async(event, context) => {
       email: email,
       message: message
     })
-      .then(res => console.log(res._id))
+      .then(res => console.log(res))
   } catch (error) {
     console.log(error)
   }
