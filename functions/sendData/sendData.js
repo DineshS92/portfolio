c43950c06e210e33d093652a5716fd5eb7feed11
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-10-07 22:11:53 +0530

    send to sanity added

---
+++ functions/sendData/sendData.js
@@ -0,0 +1,33 @@
+const sanityClient = require('@sanity/client');
+
+const client = sanityClient({
+  projectId: process.env.SANITY_PROJ,
+  dataset: process.env.SANITY_DS,
+  token: process.env.SANITY_TOKEN,
+  useCdn: false
+});
+
+
+exports.handler = async(event, context) => {
+  const {_type, name, email, message} = JSON.parse(event.body);
+  // console.log(JSON.parse(event.body));
+  try {
+    client.create({
+      _type: _type,
+      name: name,
+      email: email,
+      message: message
+    })
+      .then(console.log('success upload'))
+      return {
+        statusCode: 200,
+        body: JSON.stringify('success')
+      }
+  } catch (error) {
+    console.log(error)
+  }
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify('success')
+  // }
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,60 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+    height: 80%;
+    margin: auto 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`;
 
 export default function Contact() {
+
+  const [form, setForm] = useState({name: '', email: '', message: ''});
+
+  const handleChange = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const sendToSanity = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/.netlify/functions/sendData', {
+        method: 'POST',
+        body: JSON.stringify({
+          _type: 'form',
+          name: form.name,
+          email: form.email,
+          message: form.message
+        })
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   
   return(
     <>
-      <h1>Hi, This is the Contact Page</h1>
+      <StyledForm onSubmit={sendToSanity}>
+        <label>Name:{' '} 
+          <input type='text' required name='name' onChange={handleChange}/>
+        </label>
+        <label>Email:{' '}
+          <input type='email' required name='email' onChange={handleChange}/>
+        </label>
+        <label>Message:{' '}
+          <textarea name='message' onChange={handleChange} />
+        </label>
+        <button type='submit'>Send Message</button>
+      </StyledForm>
     </>
   );
 }
